26ec07253be49e74769ee9537a03a1c479a4d5f1
Zichao Zang <zang.code@gmail.com>
2023-07-06 12:01:35 +0100

    finished working version

--- index.js
+++ index.js
@@ -22,14 +22,18 @@ const getRandomMove = () => {
   
   // Removing elements (nodes) from the DOM
   const resetGame = () => {
+    console.log('reset game')
+
     if (document.getElementById("outcome")) {
-      const outcome = document.body.lastChild;
+        console.log('remove element')
+      const outcome = document.getElementById("outcome")
       document.body.removeChild(outcome);
     }
   };
   
   const playGame = () => {
     resetGame();
+    console.log('hi')
     const playerOneMove = getRandomMove();
     const playerTwoMove = getRandomMove();
     const outcome = getOutcome(playerOneMove, playerTwoMove);
@@ -37,8 +41,21 @@ const getRandomMove = () => {
   };
   
   const updateDOM = (moveOne, moveTwo, outcome) => {
-    // TODO Implement this method to update the DOM
-    // There are some images you can use in the images directory
+    const playerOneImg = document.getElementById('player-one-move__img')
+    playerOneImg.src = `images/${moveOne}.png`
+    const playerOneText = document.getElementById('player-one-move__name')
+    playerOneText.innerText = moveOne
+    const playerTwoImg = document.getElementById('player-two-move__img')
+    playerTwoImg.src = `images/${moveTwo}.png`
+    const playerTwoText = document.getElementById('player-two-move__name')
+    playerTwoText.innerText = moveTwo
+
+    const node = document.createElement("p");
+    node.id = 'outcome';
+    const textnode = document.createTextNode(outcome);
+    node.appendChild(textnode);
+    const playerTwo = document.getElementById("container")
+    playerTwo.after(node);
   };
   
   const playButton = document.getElementById("play-btn");
